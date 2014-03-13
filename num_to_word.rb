d59a139454dbaf877e4a7e932d5852fef555bcde
Alex Kowalczuk <askowalczuk93@gmail.com>
2014-03-12 19:08:11 -0500

    put constants into their own file

--- num_to_word.rb
+++ num_to_word.rb
@@ -1,5 +1,8 @@
 class NumToWord
 
+  # holds 1-19 and prefixes for multiples of 10
+  load "number_names.rb"
+
   def initialize(n)
     @n = n
   end
@@ -15,46 +18,6 @@ class NumToWord
       translate_num(@n).strip.squeeze(" ")
     end
   end
-
-  # dictionaries to hold words
-
-  ONE_THRU_NINETEEN =
-    {
-      # we don't want nil if we lookup zero
-      # but we also don't want "zero", so just put empty string
-      # remove excess whitespace later
-      0 => "",
-      1 => "one",
-      2 => "two", 
-      3 => "three",
-      4 => "four",
-      5 => "five",
-      6 => "six",
-      7 => "seven",
-      8 => "eight",
-      9 => "nine",
-      10 => "ten",
-      11 => "eleven",
-      12 => "twelve",
-      13 => "thirteen",
-      14 => "fourteen",
-      15 => "fifteen",
-      16 => "sixteen",
-      17 => "seventeen",
-      18 => "eighteen",
-      19 => "nineteen"
-    } 
-
-    TENS_PREFIXES = {
-      2 => "twenty",
-      3 => "thirty",
-      4 => "fourty",
-      5 => "fifty",
-      6 => "sixty",
-      7 => "seventy",
-      8 => "eighty",
-      9 => "ninety"
-    }
 
   private  
 
